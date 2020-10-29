b9f5322f7ed9f686c9c7d6399af79b10d83f08f6
mhallaq <malhallaq@gmail.com>
2020-10-29 16:07:31 +0000

    Solving the Problem in Task 4

--- tasks/exercise004.js
+++ tasks/exercise004.js
@@ -2,6 +2,44 @@
 
 function pigLatin(str) {
   // You code here!
+  const punctuationMarks = ["!", "?", ",", ";", "-", '"', "'"];
+  const strArray = str.split(" "); //convert the word string to an array of words to process it .
+  const modifiedArray = [];
+
+  // Function to get the position of the punctuation Mark in a word
+  function getPunctuationPosition(word) {
+    let wordPunctuationPosition = -1;
+    for (let i = 0; i < punctuationMarks.length; i++) {
+      wordPunctuationPosition = word.indexOf(punctuationMarks[i]);
+      if (wordPunctuationPosition != -1) break;
+    }
+    return wordPunctuationPosition;
+  }
+
+  // function to modify the word according to the condition provided in the task
+
+  function modifyWord(word) {
+    let temp = word;
+    let modifiedWord;
+    const punctuationPosition = getPunctuationPosition(word);
+    if (punctuationPosition === -1) {
+      modifiedWord = temp.slice(1) + word.substring(0, 1) + "ay";
+    } else {
+      modifiedWord =
+        temp.slice(1, punctuationPosition) +
+        word.substring(0, 1) +
+        "ay" +
+        word.substring(punctuationPosition);
+    }
+
+    return modifiedWord;
+  }
+
+  // loop through the provided words and convert it
+  for (let i = 0; i < strArray.length; i++) {
+    modifiedArray.push(modifyWord(strArray[i]));
+  }
+  return modifiedArray.join(" ");
 }
 
 module.exports = {
